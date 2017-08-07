0c83a669dd22e24d48e6121cc0a2586f66f14dd0
“Alice <aliceletourneur@icloud.com>
2017-08-07 23:54:25 +0100

    Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has a URL defined which is not null', function() {
+           allFeeds.forEach(function(feed) {
+             // Test if a url is defined for each item in the allFeeds array
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+          });
+         });
 
 
         /* TODO: Write a test that loops through each feed
