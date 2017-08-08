3ebba61e8d7a60e85f3391e2e0a0bb8d1330a756
“Alice <aliceletourneur@icloud.com>
2017-08-08 19:51:48 +0100

    Update commented text to make the file more readable

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,36 +1,15 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+// Placing all our tests within the $() function to ensure they don't run until the DOM is ready
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    // This test suite contains a set of tests about RSS feeds.
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
+        // Tests if allFeeds variable has been defined and that it is not empty
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        // Test if all allFeeds items have a URL defined and that the URL is not empty
          it('has a URL defined which is not empty', function() {
            allFeeds.forEach(function(feed) {
              // Test if a url is defined for each item in the allFeeds array
@@ -40,11 +19,7 @@ $(function() {
           });
          });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+         // Test if all allFeeds items have a name defined and that the name is not empty
          it('has a name defined which is not empty', function() {
            allFeeds.forEach(function(feed) {
              // Test if a name is defined for each item in the allFeeds array
